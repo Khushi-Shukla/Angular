596b3ff6dedaaafe6ceb2b0d36707c946ca9a3e3
Khushi-Shukla <tusharika.shukla05@gmail.com>
2020-09-19 17:29:24 +0530

    Dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import {  MatListModule } from '@angular/material/list'
-import {  MatGridListModule } from '@angular/material/grid-list'
-import {  MatCardModule } from '@angular/material/card'
-import {  MatButtonModule } from '@angular/material/button'
-import { FlexLayoutModule} from '@angular/flex-layout'
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {  MatListModule } from '@angular/material/list';
+import {  MatGridListModule } from '@angular/material/grid-list';
+import {  MatCardModule } from '@angular/material/card';
+import {  MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
@@ -24,6 +25,7 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -35,7 +37,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent   
+    ContactComponent,
+    LoginComponent   
   ],
   imports: [
     BrowserModule,
@@ -46,12 +49,16 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
